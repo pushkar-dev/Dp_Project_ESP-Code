9f33b07a04e9e6292e9e4dc39d8e78bdaf24e7b6
amitmaindola <amitkm.knp2019@gmail.com>
2022-08-27 23:06:56 +0530

    added some functionalities

--- Memory_dp.hpp
+++ Memory_dp.hpp
@@ -2,15 +2,13 @@
 
 int slots[7][3];
 
-// inital values 
-
+// inital values
 void initSlot(){
   for(int i=0; i<7; i++){
-    for(int j=0; j<3; j++){
-      slots[i][j]=2500; // setting default values to 25:00 so that it never matches
-    }
+      slots[i][0]=800; // setting default values to 25:00 so that it never matches
+      slots[i][1]=1700;
+      slots[i][2]=2200;
   }
-  
   // Adding initial slots to EEPROM
   for (int i = 0 ; i < 21 ; i++) {
       EEPROM.write(i, slots[i/3][i%3]);
@@ -18,6 +16,12 @@ void initSlot(){
   EEPROM.write(25,1); // set 1 as it is initialized
 }
 
+// Loading slots from EEPROM to slots[][]
+void loadSlot(){
+  for (int i = 0 ; i < 21 ; i++) {
+      slots[i/3][i%3] = EEPROM.read(i);
+  }
+}
 
 // Updating any slot
 void update(int d, int s, int time){
@@ -25,15 +29,28 @@ void update(int d, int s, int time){
   EEPROM.write(d*3+s, time);
 }
 
+// Geting the time of a slot
+int getSlot(int d, int s){
+  return slots[d][s];
+}
+
+// To get status of led
+bool[] getStatus(int d, int s){
+  int num = d*3 + s;
+  int arr[5];
+  for(int i=0; i<=4; i++){
+    arr[i]=num%2;
+    num=num/2;
+  }
+  return arr;
+}
+
 // sets up memory
 void setup_mem()
 {
   int isSet = EEPROM.read(25);
   if(!isSet) initSlot();
-  // Adding initial slots to EEPROM
-  for (int i = 0 ; i < 21 ; i++) {
-      slots[i/3][i%3] = EEPROM.read(i);
-  }
+  else loadSlot();
 }
 
 
@@ -41,7 +58,6 @@ void time_loop(int day, int curr_t)
 {
   for(int s=0; s<3; s++){
     if(slots[day][s]<=curr_t && curr_t<slots[day][s]+2){
-      //Serial.println("Matched");
       beepStart();
       ledStart(day, s);
       delay(6000);
